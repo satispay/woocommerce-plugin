1e670124a92de3a917381036d3d11195759950d0
Stiven Masha <stiven.masha@satispay.com>
2022-10-24 17:27:39 +0200

    Update scripts

--- scripts/generate-pot.sh
+++ scripts/generate-pot.sh
@@ -2,3 +2,4 @@ mkdir tmp
 curl https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar -o tmp/wp-cli.phar
 php tmp/wp-cli.phar i18n make-pot . tmp/woo-satispay.pot --slug=woo-satispay --exclude=tmp-svn,tmp-plugin
 cp tmp/woo-satispay.pot .
+rm -rf tmp
--- scripts/svn.sh
+++ scripts/svn.sh
@@ -1,11 +1,9 @@
-# ./generate-pot.sh
+sh scripts/generate-pot.sh
 
-# rm -rf tmp-svn
-# svn co https://plugins.svn.wordpress.org/woo-satispay tmp-svn
+rm -rf tmp-svn
+svn co https://plugins.svn.wordpress.org/woo-satispay tmp-svn
 (cd tmp-svn/trunk && rm -rf *)
-# cp -R images tmp-svn/trunk
 cp -R satispay-sdk tmp-svn/trunk
-# cp -R languages tmp-svn/trunk
 cp LICENSE tmp-svn/trunk
 cp readme.txt tmp-svn/trunk
 cp logo.png tmp-svn/trunk
@@ -15,6 +13,6 @@ cp woo-satispay.pot tmp-svn/trunk
 
 echo "\nnext manual commands:"
 echo "  (cd tmp-svn && svn add trunk/**/* && tmp-svn svn stat)"
-echo "  (cd tmp-svn && svn ci --username satispay -m 'update')"
+echo "  (cd tmp-svn && svn ci --username satispay -m 'My changelog')"
 echo "  (cd tmp-svn && svn cp trunk tags/x.x.x)"
-echo "  (cd tmp-svn && svn ci --username satispay -m 'bump version')"
+echo "  (cd tmp-svn && svn ci --username satispay -m 'Created tag x.x.x')"
