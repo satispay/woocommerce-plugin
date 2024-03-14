e2303c5d687f8b9dd88d32af1520341e673dc559
Gabriele <francesco.chiaramonte@satispay.com>
2024-03-14 15:46:28 +0100

    Script
    - Updated Script for package creation

--- scripts/generate-package-zip.sh
+++ scripts/generate-package-zip.sh
@@ -7,5 +7,8 @@ cp -R .gitignore ~/Downloads/woo-satispay
 cp -R LICENSE ~/Downloads/woo-satispay
 cp -R readme.txt ~/Downloads/woo-satispay
 cp -R satispay-sdk ~/Downloads/woo-satispay
+cp -R assets  ~/Downloads/woo-satispay
+cp -R includes ~/Downloads/woo-satispay
+cp -R resources ~/Downloads/woo-satispay
 cd ~/Downloads && find . -name ".DS_Store" -delete
 zip -r woo-satispay.zip woo-satispay -x ".*" -x "__MACOSX"
